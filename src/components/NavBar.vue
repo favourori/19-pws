12d181f370b32f76cacab9cf9b76d900fdbf4ccf
auztincodes <austinugo200@gmail.com>
2020-05-02 14:02:19 +0100

    made nav responsive

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,32 +4,46 @@
       <div class="nav-wrapper container">
         <router-link :to="{ name: 'home' }">
           <span
-            class="brand-logo animated fadeIn"
+            class="brand-logo animated fadeIn left"
             style="animation-duration: 3s;"
           >
             FAVOUR ORI<span class="secondary-color">.</span>
           </span>
         </router-link>
-
-      
+        <a href="#" data-target="mobile-demo" class="sidenav-trigger right"
+          ><i class="material-icons">menu</i></a
+        >
         <ul id="nav-mobile" class="right hide-on-med-and-down">
-          <router-link :to="{ name: 'projects' }">
-            Featured projects
-          </router-link>
+          <li>
+            <router-link :to="{ name: 'projects' }">
+              Featured projects
+            </router-link>
+          </li>
+          <li>
+            <router-link :to="{ name: 'about' }">
+              About me
+            </router-link>
+          </li>
         </ul>
-
-          <ul id="nav-mobile" class="right hide-on-med-and-down">
-          <router-link :to="{ name: 'about' }">
-            About me
-          </router-link>
-        </ul>
-
       </div>
     </nav>
+    <ul class="sidenav" id="mobile-demo">
+      <li @click="hideSideBar()">
+        <router-link :to="{ name: 'projects' }">
+          Featured projects
+        </router-link>
+      </li>
+      <li @click="hideSideBar()">
+        <router-link :to="{ name: 'about' }">
+          About me
+        </router-link>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import M from "materialize-css";
 export default {
   name: "NavBar",
 
@@ -37,6 +51,14 @@ export default {
     return {
       isHome: true,
     };
+  },
+  mounted() {
+    M.AutoInit();
+  },
+  methods: {
+    hideSideBar() {
+      document.querySelector(".sidenav-overlay").click();
+    },
   },
 };
 </script>
@@ -51,5 +73,18 @@ export default {
   color: #66bd90;
   font-weight: 700;
   font-size: 40px;
+}
+.sidenav {
+  background-color: #000 !important;
+  padding: 20px;
+}
+.sidenav li > a {
+  color: rgba(255, 255, 255, 0.87);
+}
+@media only screen and (max-width: 992px) {
+  nav .brand-logo.left {
+    line-height: 0.9;
+    font-size: 20px;
+  }
 }
 </style>
